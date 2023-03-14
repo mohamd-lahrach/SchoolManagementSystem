f878dfc2324e081f235f42e89250ab6d2257a6d5
mohamd lahrach <mohamedlahrach00@gmail.com>
2023-03-14 14:19:40 +0100

    fist commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,20 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
-
 repositories {
+    // Add the Firebase Admin SDK repository
     mavenCentral()
+    maven {
+        url = uri("https://firebase.google.com/download/admin-sdk/maven")
+    }
 }
+
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation ("com.google.firebase:firebase-admin-kotlin:1.7.0")
+    implementation ("com.google.firebase:firebase-admin:8.2.0")
+
 }
 
 tasks.test {
--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,3 +1,17 @@
-fun main(args: Array<String>) {
+package main
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import java.io.FileInputStream
+
+fun main(args: Array<String>) {
+    val serviceAccount = FileInputStream("src/main.main/resource/student-management-syste-d1f7e-firebase-adminsdk-h198d-c07b43d10a.json")
+
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl("https://student-management-syste-d1f7e-default-rtdb.firebaseio.com/")
+        .build()
+
+    FirebaseApp.initializeApp(options)
 }
